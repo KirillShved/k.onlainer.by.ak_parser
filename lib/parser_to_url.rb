a6f6a467403708d69fc52e0cdf981e6249165b5a
«kirillshved» <kirillshved@coherentsolutions.com>
2017-04-27 15:21:47 +0300

    fixed codestyle

--- lib/parser_to_url.rb
+++ lib/parser_to_url.rb
@@ -14,17 +14,17 @@ class ParserToUrl
   PAGES_LIMIT = 25
   ITEMS_LIMIT = 30
 
-  attr_reader :url, :url_params, :array_urls, :page_count, :number_page
+  attr_reader :url, :url_params, :array_urls, :page_count
 
   def initialize(url_params = {})
     @url_params = url_params
     @array_urls = []
     @page_count = 0
-    @number_page = 1
   end
 
   def solutions
     array_urls
+    puts 'Congratilation, It`s alive!'.freeze
   end
 
   private
@@ -35,13 +35,12 @@ class ParserToUrl
 
   def total_pages
     input_options
-    puts sum = Nokogiri::HTML(open(@url)).to_s.scan(TOTAL_REGULAR).join.to_i
+    sum = Nokogiri::HTML(open(@url)).to_s.scan(TOTAL_REGULAR).join.to_i
     @page_count = (sum / ITEMS_LIMIT.to_f).ceil.clamp(1, PAGES_LIMIT)
   end
 
   def array_urls
     total_pages
-    puts @page_count
     (1..@page_count).each do |page_number|
       array = Nokogiri::HTML(open(@url.gsub('page=1', "page=#{page_number}"))).to_s.scan(REGULAR).map do |url|
         url.gsub('\/', '/')
